2a36218fea67a3981db602cc1d03f8e6968d77d7
treuther <tracyreuther@gmail.com>
2020-04-04 11:52:24 -0500

    general steps typed out

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <button>Increase all</button>
+        <button>Decrease all</button>
           <Counter />
           <Counter />
           <Counter />
@@ -22,3 +24,11 @@ export default App;
 // 1. Page has 2 extra buttons
 // a. Clicking on "increase all" will increase the count by 1 on all the counters
 // b. Clicking on "decrease all" will decrease the count by 1 on all the counters
+
+// STEPS
+
+// 1. create a button for increase all and decrease all
+// 2. create an empty function for corres. button
+// 3. add an event handler to each button referencing corres. function
+// 4. Reference the childComponent - use createRef in constructor
+// 5. Add ref to <Counter />
