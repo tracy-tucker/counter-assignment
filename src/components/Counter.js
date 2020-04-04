02348130319ab2046f1e565309b8eaabd4fd2898
treuther <tracyreuther@gmail.com>
2020-04-04 10:27:22 -0500

    writing out steps for lab

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,10 @@ import React from 'react';
 
 class Counter extends React.Component {
 
+    state = {
+        num: 0,
+    }
+
     render() {
         return (
             <div>
@@ -13,3 +17,24 @@ class Counter extends React.Component {
 }
 
 export default Counter;
+
+
+
+// Create a Counter
+
+// 1. Create a plus button and a minus button
+// a. Clicking on plus increases count, click on minus decreases count.
+
+// BONUS
+
+// 1. Page has 2 extra buttons
+// a. Clicking on "increase all" will increase the count by 1 on all the counters
+// b. Clicking on "decrease all" will decrease the count by 1 on all the counters
+
+// STEPS
+
+// 1. create button elements - one for + and other for -
+// 2. set the initial state to zero
+// 2. create 2 functions - one for increment, one for decrement
+// 3a. add event change handler to each button
+// 3b. call the function (include function name) for each corresponding button
