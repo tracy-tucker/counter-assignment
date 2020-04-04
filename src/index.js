25e87ca7a049f65779f59d1ec409227d9ca0ee54
treuther <tracyreuther@gmail.com>
2020-04-04 06:54:16 -0500

    create folder and counter component

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Counter />
+      <Counter />
+      <Counter />
+    </App>
   </React.StrictMode>,
   document.getElementById('root')
 );
