48f6c698ed5859ff6af99274bfa08197def8b39b
treuther <tracyreuther@gmail.com>
2020-04-04 11:25:05 -0500

    testing new functions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -47,6 +47,7 @@ export default Counter;
 // 1. Page has 2 extra buttons
 // a. Clicking on "increase all" will increase the count by 1 on all the counters
 // b. Clicking on "decrease all" will decrease the count by 1 on all the counters
+// SEE APP FOR FOR BONUS STEPS
 
 // STEPS
 
